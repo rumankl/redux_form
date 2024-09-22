988c21896f2a2d9ff8b1caaa60e5f404160ae771
news <rumankrishnalohala@gmail.com>
2024-09-22 14:42:50 +0545

    adding validation email and title

--- src/features/posts/AddForm.jsx
+++ src/features/posts/AddForm.jsx
@@ -5,25 +5,35 @@ import { checkData, radioData } from '../shared/data'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router'
 import { addPost } from './postSlice'
+import * as Yup from 'yup';
 
+
+const valSchema = Yup.object({
+  title:Yup.string().max(10).min(4).required(),
+   details:Yup.string().matches(/^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i, 'please provide valid email ').required(),
+  // program:'',
+  // genres:[],
+  // country:'',
+  // image:[]
+})
 const AddForm = () => {
   const dispatch= useDispatch();    ///action call garney ko lagi yo.
   const nav =useNavigate();///previous, next ,logical , interaction, p-italic page haru pathai na ko.
 
-  const {handleChange,values,handleSubmit,setFieldValue}= useFormik({
+  const {handleChange,values,handleSubmit,setFieldValue, errors}= useFormik({
     initialValues:{
             title:'',
             details:'',
             program:'',
             genres:[],
             country:'',
-            image:''
+            image:[],
           },
     onSubmit:(val=>{
       dispatch(addPost(val));
       nav(-1); ///previous page ra back jana use -1 .
-    })
-
+    }),
+validationSchema : valSchema
   });
 //  console.log(URL); //for review ko lagi createobjecturl use garney 
   //  console.log(values.image); //this place is perfect to rerenders the fileupload path
@@ -32,7 +42,8 @@ const AddForm = () => {
     <div className='p-7 max-w-[300px]' >
       <form className='space-y-4' onSubmit={handleSubmit}>
       <Input name='title' onChange={handleChange} value={values.title} label='Title' />
-      
+
+      <h1 className='text-red-200'>{errors.title}</h1>
 
       <div className="flex gap-10">
       {radioData.map((val,i)=>{
@@ -58,12 +69,14 @@ const AddForm = () => {
         <Option value='China'>China</Option>
       </Select>
     </div>
-    <Textarea name='details' onChange={handleChange} value={values.details} label='Detail'/>
+    <Textarea name='details' onChange={handleChange} value={values.details} label='Detail'/>      
+    <h1 className='text-red-200'>{errors.details}</h1>
+
     <Input name='image' onChange={(e)=>{
     // console.log(e.target.files[0]);
     
          const file = e.target.files[0]; //e.target.files = is a filelist object so URL.createObjectURL canot handle dirctly.hence, e.target.files[0] is suitable to write
-         
+
         //  setFieldValue('image', URL.createObjectURL(file)); 
 
         //  console.log(values.image); it is not suitable to write here beacause it cannot rerenders.
